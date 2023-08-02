81245d81fd0ec34442afd11f9c82c0c7a105904b
OverKill DEV <140531897+overkillzero@users.noreply.github.com>
2023-08-02 09:42:56 +0700

    Update crack.sh

--- crack.sh
+++ crack.sh
@@ -1,5 +1,3 @@
-#Crack aaPanel
 echo "Đang Crack, vui lòng chờ"
 sed -i 's|"endtime": -1|"endtime": 999999999999|g' /www/server/panel/data/plugin.json
 sed -i 's|"pro": -1|"pro": 0|g' /www/server/panel/data/plugin.json
-echo "Crack hoàn tất, vui lòng đăng nhập lại aaPanel"
